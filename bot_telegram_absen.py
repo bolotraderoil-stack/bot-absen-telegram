d75c2c56dec272846a4726095877f3697d6c1b73
bolotraderoil-stack <bolotraderoil@gmail.com>
2026-06-25 23:27:52 +0700

    Update bot_telegram_absen.py

--- bot_telegram_absen.py
+++ bot_telegram_absen.py
@@ -51,7 +51,7 @@ def home():
         conn.close()
 
         navbar = """<nav style="background:#4CAF50;padding:15px;text-align:center">
-      #  <a href="/" style="color:white;margin:0 20px;text-decoration:none;font-weight:bold">📋 Absensi</a>
+        
         <a href="/genset" style="color:white;margin:0 20px;text-decoration:none;font-weight:bold">⛽ Genset BBM</a></nav>"""
 
         option_nama = '<option value="">Semua Karyawan</option>'
